336f8649411aae2892e0e1014fd63d9297fd9903
Thanh Nguyen <huytnguy@microsoft.com>
2019-05-07 22:56:13 -0700

    Added homework3

---
+++ Homework3_ListViewSort/ListViewWPF/Homework3.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+
+namespace ListViewWPF
+{
+    /// <summary>
+    /// Interaction logic for Homework3.xaml
+    /// </summary>
+    public partial class Homework3 : Window
+    {
+
+
+        private GridViewColumnHeader listViewSortCol = null;
+        public Homework3()
+        {
+            InitializeComponent();
+
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "2DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "1StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+        }
+
+        private void uxListColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            //clear any sort criteria
+            uxList.Items.SortDescriptions.Clear();
+
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+
+
+            
+            uxList.Items.SortDescriptions.Add(new SortDescription(sortBy, ListSortDirection.Ascending));
+        }
+    }
+}
---
+++ Homework3_ListViewSort/ListViewWPF/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using ListViewWPF;
+
+
+
+namespace ListViewWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class SortAdorner : Adorner
+        {
+            private static Geometry ascGeometry =
+                Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+            private static Geometry descGeometry =
+                Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+            public ListSortDirection Direction { get; private set; }
+
+            public SortAdorner(UIElement element, ListSortDirection dir)
+                : base(element)
+            {
+                this.Direction = dir;
+            }
+
+            protected override void OnRender(DrawingContext drawingContext)
+            {
+                base.OnRender(drawingContext);
+
+                if (AdornedElement.RenderSize.Width < 20)
+                    return;
+
+                TranslateTransform transform = new TranslateTransform
+                    (
+                        AdornedElement.RenderSize.Width - 15,
+                        (AdornedElement.RenderSize.Height - 5) / 2
+                    );
+                drawingContext.PushTransform(transform);
+
+                Geometry geometry = ascGeometry;
+                if (this.Direction == ListSortDirection.Descending)
+                    geometry = descGeometry;
+                drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+                drawingContext.Pop();
+            }
+        }
+        //private GridViewColumnHeader listViewSortCol = null;
+        private GridViewColumnHeader listViewSortCol = null;
+        //private SortAdorner listViewSortAdorner = null;
+        private SortAdorner listViewSortAdorner = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<myUser> items = new List<myUser>();
+            items.Add(new myUser() { Name = "John Doe", Age = 42, Email = "JohnDoe@gamajamamail.com"});
+            items.Add(new myUser() { Name = "Jane Doe", Age = 42, Email = "JaneDoe@hotmail.com"});
+            items.Add(new myUser() { Name = "Sammy Doe", Age = 13, Email = "SammyDoe@gmail.com" });
+            
+            lvUsers.ItemsSource = items; //lvUsers is the name of the list view
+            //CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvUsers.ItemsSource);
+            //CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvUsers.ItemsSource); //instanctiate a CollectionView called view
+            //view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
+            //view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
+            //view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));//multiple sort criterias can be done by .adding a new SortDescription instance
+
+
+        }
+
+        public class myUser
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Email { get; set; }
+
+            public override string ToString()
+            {
+                return this.Name + " " + this.Age + " " + this.Email;
+            }
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader); //type sets from XAML representation to C# representation
+            string sortBy = column.Tag.ToString(); //grabs the tag of column
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                lvUsers.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            lvUsers.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+
+        }
+
+     
+    }
+}
